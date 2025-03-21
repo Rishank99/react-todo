97b9bf2cecb92105987396024b8b1e9a52a2bc30
Rishank99 <111288921+Rishank99@users.noreply.github.com>
2025-03-22 02:46:28 +0530

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -12,53 +12,23 @@ export const Todo = () => {
     document.title = "ToDo-LiSt";
   }, []);
 
-  // 2
-
-  // 5
   const [task, setTask] = useState(() => getLocalStorageTodoData());
 
-  // 14
-
-  // 3
-
-  // 4
   const handleFormSubmit = (inputValue) => {
-    //Destructuring inputValue object
     const { id, content, checked } = inputValue;
-    // 6 To check if the input field is empty or not
     if (!content) return;
-
-    // 8
-    // if (task.includes(inputValue)) return;
-    // 10 To check if the data is already existing or not
-    // if (task.includes(inputValue)) {
-    //   return;
-    // }
-
-    // To check if the data is already existing or not
     const ifTodoContentMatched = task.find(
       (curTask) => curTask.content === content
     );
     if (ifTodoContentMatched) return;
-
-    // 7
     setTask((prevTask) => [
       ...prevTask,
       { id: id, content: content, checked: checked },
     ]);
-
-    // 9
   };
 
   setLocalStorageTodoData(task);
 
-  // 13 For Date and Time
-  // const date = new Date();
-  // const formattedDate = date.toLocaleDateString();
-  // const formattedTime = date.toLocaleTimeString();
-  // 16
-
-  // 18
   const handleDeleteTodo = (value) => {
     const updatedTask = task.filter((curElem) => {
       return curElem.content != value;
@@ -66,7 +36,6 @@ export const Todo = () => {
     setTask(updatedTask);
   };
 
-  // 20
   const handleClearTodoData = () => {
     setTask([]);
   };
@@ -82,16 +51,13 @@ export const Todo = () => {
     setTask(updatedTask);
   };
 
-  // 1
   return (
     <section className="todo-container">
       <header>
         <h1>Todo List</h1>
-        {/* 12 */}
         <TodoDate />
       </header>
       <TodoForm onAddTodo={handleFormSubmit} />
-      {/* 11 */}
       <section className="myUnOrdList">
         <ul>
           {task.map((curTask) => {
@@ -107,7 +73,6 @@ export const Todo = () => {
           })}
         </ul>
       </section>
-      {/* 19 */}
       <section>
         <button className="clear-btn" onClick={handleClearTodoData}>
           Clear all

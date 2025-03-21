0d80d0ac9b58576a8f86b4d97c104e9d38017101
Rishank99 <111288921+Rishank99@users.noreply.github.com>
2025-03-22 02:47:08 +0530

    Update TodoDate.jsx

--- src/components/TodoDate.jsx
+++ src/components/TodoDate.jsx
@@ -6,18 +6,13 @@ export const TodoDate = () => {
       const date = new Date();
       const formattedDate = date.toLocaleDateString();
       const formattedTime = date.toLocaleTimeString();
-
-      // 15
       setDateTime(`${formattedDate} - ${formattedTime}`);
     }, 1000);
-
-    // 17 useEffect clean up function
     return () => clearInterval(interval);
   }, []);
+  
   return (
     <h2 className="date-time">
-      {/* {formattedDate} - {formattedTime} */}
-      {/* 16 */}
       {dateTime}
     </h2>
   );
